c97e3ff57012cc51798927f23ca3ef8aab895c09
John <jun0205@foxmail.com>
2021-03-03 12:24:56 +0800

    Update

--- add_passwall.sh
+++ add_passwall.sh
@@ -7,7 +7,7 @@ rm -rf feeds/lienol/pdnsd-alt
 rm -rf feeds/lienol/lienol/luci-app-verysync
 rm -rf feeds/lienol/package/verysync
 rm -rf feeds/lienol/package/chinadns-ng
-rm -rf package/lean/openwrt-chinadns-ng
+#rm -rf package/lean/openwrt-chinadns-ng
 rm -rf package/lean/luci-app-kodexplorer
 rm -rf package/lean/luci-app-pppoe-relay
 rm -rf package/lean/luci-app-pptp-server
