d428b6e6fca91be04be01e74d660c029da3efe39
John <jun0205@foxmail.com>
2021-03-03 15:05:02 +0800

    Update

--- add_passwall.sh
+++ add_passwall.sh
@@ -4,12 +4,12 @@ echo -e '\nsrc-git lienol https://github.com/xiaorouji/openwrt-passwall' >> feed
 rm -rf feeds/lienol/ipt2socks
 rm -rf feeds/lienol/shadowsocksr-libev
 rm -rf feeds/lienol/pdnsd-alt
-rm -rf feeds/lienol/lienol/luci-app-verysync
-rm -rf feeds/lienol/package/verysync
-rm -rf feeds/lienol/package/chinadns-ng
+rm -rf feeds/lienol/chinadns-ng
 #rm -rf package/lean/openwrt-chinadns-ng
 rm -rf package/lean/luci-app-kodexplorer
 rm -rf package/lean/luci-app-pppoe-relay
 rm -rf package/lean/luci-app-pptp-server
 rm -rf package/lean/luci-app-v2ray-server
+rm -rf package/lean/luci-app-verysync
+rm -rf package/lean/verysync
 ./scripts/feeds install -a -p lienol
